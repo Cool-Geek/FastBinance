9f6274129f8012eb476f755842da89ead6eaa730
Cool-Geek <bh.cool.geek@gmail.com>
2021-05-12 18:27:36 +0430

    comment main method to build library

--- FastBinance/Program.cs
+++ FastBinance/Program.cs
@@ -1,26 +1,26 @@
-﻿using FastBinance.BinanceApi;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using FastBinance.BinanceApi;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace FastBinance
-{
-    class Program
-    {
-        public async static Task Main(string[] args)
-        {
-            BinanceClient client = new BinanceClient("aFcKiRkCD0g9CmtsEQdach6kKR1N7wtAGu2qRHfEaczVGFjlcL1531KvHrGSYvSp", "MfmVBSgIT54oTWfUyVzQHbhkhHdHual1HPUWAPMgsaBERvKxKUI5KkaB3Yby7DQT");
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+//namespace FastBinance
+//{
+//    class Program
+//    {
+//        public async static Task Main(string[] args)
+//        {
+//            BinanceClient client = new BinanceClient("aFcKiRkCD0g9CmtsEQdach6kKR1N7wtAGu2qRHfEaczVGFjlcL1531KvHrGSYvSp", "MfmVBSgIT54oTWfUyVzQHbhkhHdHual1HPUWAPMgsaBERvKxKUI5KkaB3Yby7DQT");
+//            var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            var order = await client.GetSymbolMarketDepth("BTCUSDT", 10);
+//            var order = await client.GetSymbolMarketDepth("BTCUSDT", 10);
 
 
 
-            watch.Stop();
-            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
-            Console.ReadKey();
-        }
-    }
-}
+//            watch.Stop();
+//            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+//            Console.ReadKey();
+//        }
+//    }
+//}
